b67817a128b136d2c3d979f6816c80f09959e7a7
Yatesudev <yatesu0x00@gmail.com>
2024-06-07 19:00:03 +0200

    inital setup with mongoose + app.module.ts

--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const { MongoClient, Db } = require('mongodb');
+const mongoose = require('mongoose');
 require('dotenv').config(); // Load environment variables from .env file
 
 const mongoURI = process.env.MONGODB_URI;
-const dbName = process.env.MONGODB_DB;
 
 app.use(express.static(path.join(__dirname, '../dist/mind-quest-leveling/browser')));
 
@@ -14,21 +13,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/mind-quest-leveling/browser/index.html'));
 });
 
-MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(async client => {
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     console.log('Connected to MongoDB');
-
-    const db = client.db(dbName); // Get the database instance
-
-      /* Insert test data into your collection
-      const collection = db.collection("Test"); // Get the collection within the database
-      await collection.insertMany([
-        { name: 'Test Data 1', value: 123 },
-        { name: 'Test Data 2', value: 456 },
-        { name: 'Test Data 3', value: 789 }
-      ]);*/
-
-    client.close(); // Close the connection once done
   })
   .catch(err => console.error('Could not connect to MongoDB', err));
 
